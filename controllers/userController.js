43f42ce8b8a6cef21902951634cf87da251d34a6
emiliopini <epini@udesa.edu.ar>
2025-04-08 16:41:45 -0300

    Error en db

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,13 @@
 const db = require("../db/dataBase");
 const userController = {
     login: (req, res) => res.render('login.ejs'),
+
     register: (req, res) => res.render('register.ejs'),
+
     profile: (req, res) => res.render('profile.ejs', { usuario: db.perfil.usuario, 
         correo: db.perfil.email, 
         foto: db.perfil.fotoPerfil }),
+        
     buscar: (req, res) => res.render('search-results.ejs'),
 }
 
